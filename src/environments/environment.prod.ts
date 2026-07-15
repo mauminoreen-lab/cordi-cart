0612e2fc6a8eece7857708b79985c752e2226106
mauminoreen-lab <mauminoreen-lab@users.noreply.github.com>
2026-07-15 23:03:52 +0800

    Set production API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://cordi-cart.onrender.com'
+  apiUrl: 'https://cordi-cart.onrender.com'  
 };
